626c6487d9219d321bed94a42a13435d7a8e1d1b
Tumpes <tumpesiili@gmail.com>
2023-12-02 11:57:01 +0200

    Changed mincolor class for a dict

--- day2/solve2.py
+++ day2/solve2.py
@@ -1,10 +1,5 @@
 #!usr/bin/python
 
-class Mincolor:
-	r = 0
-	b = 0
-	g = 0
-
 file = open("input.txt", "r")
 data = file.read()
 file.close()
@@ -21,21 +16,17 @@ for i in range(len(data)):
 	cubes = cubes.split(",")
 	game_id = int(cubes[0].split()[1])
 	cubes.pop(0)
-
-	mincolor = Mincolor()
+	
+	mincolor = {"r": 0, "b": 0, "g": 0}
 
 	for cube in cubes:
 		amount = int(cube.split()[0])
 		color = cube.split()[1][0]
 
-		if color == "r" and amount > mincolor.r:
-			mincolor.r = amount
-		if color == "b" and amount > mincolor.b:
-			mincolor.b = amount
-		if color == "g" and amount > mincolor.g:
-			mincolor.g = amount
+		if color in mincolor and amount > mincolor[color]:
+			mincolor[color] = amount
 
-	power = mincolor.r * mincolor.b * mincolor.g
+	power = mincolor["r"] * mincolor["b"] * mincolor["g"]
 	__sum += power
 
 
